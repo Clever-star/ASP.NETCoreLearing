4c71b62043f499af44d93fa61c36e26051108f80
Clever-star <1908282601@qq.com>
2021-02-03 21:00:26 +0800

    这是P26

--- Routine.Api/Models/CompanyAddDto.cs
+++ Routine.Api/Models/CompanyAddDto.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Routine.Api.Models
 {
-    //该类专门用于新增创建
-    //【注】尽管有时候用于创建和查询的Dto是一样的，也要分为不同的Dto，因为业务需求是动态变化的，分为两个类便于重构
     public class CompanyAddDto
     {
-        //不需要传入实体中的Id属性
+        [Display(Name = "公司名")]
+        [Required(ErrorMessage ="{0}这个字段是必填的")]
+        [MaxLength(100,ErrorMessage ="{0}的最大长度不可以超过{1}")]
         public string Name { get; set; }
+
+        [Display(Name = "简介")]
+        [StringLength(500,MinimumLength =10,ErrorMessage ="{0}的最大长度范围从{2}到{1}")]
         public string Introduction { get; set; }
-        public ICollection<EmployeeAddDto> Employees { get; set; } = new List<EmployeeAddDto>();//这样可以避免发生空引用异常
-        //该属性（Employees）与Comany中的导航属性Employees相同
+        public ICollection<EmployeeAddDto> Employees { get; set; } = new List<EmployeeAddDto>();
     }
 }
