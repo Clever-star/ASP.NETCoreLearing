ca3b0ca12d66a4adedfab19fd66222b380d72916
Clever-star <1908282601@qq.com>
2021-02-03 20:06:19 +0800

    这是P24

--- Routine.Api/Controllers/CompanyCollectionController.cs
+++ Routine.Api/Controllers/CompanyCollectionController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Routine.Api.Entities;
+using Routine.Api.Helpers;
 using Routine.Api.Models;
 using Routine.Api.Services;
 using System;
@@ -24,6 +25,39 @@ namespace Routine.Api.Controllers
             _companyRepository = companyRepository ?? throw new ArgumentNullException(nameof(companyRepository));
         }
 
+        //需要接收一组key
+        //写法一：1,2,3,4
+        //写法二：key1=value1,key2=value2,key3=value3
+        [HttpGet("{ids}",Name = nameof(GetCompanyCollection))]
+        public async Task<IActionResult> GetCompanyCollection(
+        [FromRoute]
+        [ModelBinder(BinderType = typeof(ArrayModelBinder))]
+        IEnumerable<Guid> ids)
+        //如果不写[FromRoute]就默认从body中获得，但get请求不应该有请求body
+        //像这种集合类的参数是无法从路由中获得的，因为路由是逗号分开的字符串。现在，仅通过隐式的绑定无法从路由中获得ID的集合
+        //ASP.NET core允许我们创建自定义的model绑定，建立一个helpers文件夹
+        {
+            //首先判断Id参数是否为null
+            if (ids == null)
+            {
+                //为null时，说明请求不合理，属于客户端的错误，返回BadRequest
+                return BadRequest();
+            }
+
+            //如果不为空
+            var entities = await _companyRepository.GetCompaniesAsync(ids);
+            //只有当查询的个数和找到的个数相等时，才算查询成功
+            if (ids.Count() != entities.Count())
+            {
+                return NotFound();
+            }
+
+            //把company的实体转换为dto类 返回前端展示。
+            var dtosToReturn = _mapper.Map<IEnumerable<CompanyDto>>(entities);
+            return Ok(dtosToReturn);
+        }
+
+
         [HttpPost]
         //返回结果是一个集合
         public async Task<ActionResult<IEnumerable<CompanyDto>>> CreateCompanyCollection(
@@ -37,8 +71,16 @@ namespace Routine.Api.Controllers
 
             await _companyRepository.SaveAsync();
 
-            //return CreatedAtRoute();//因为没有get方法（当前companycollection没有key）所以无法返回
-            return Ok();
+            //POST成功后，需要返回成功的资源，作为返回的第三个参数
+            var dtosToReturn = _mapper.Map<IEnumerable<CompanyDto>>(companyEntities);
+
+            //第二个参数是egt方法所需的参数，在这里是ids
+            var idsString = string.Join(",", dtosToReturn.Select(x => x.Id));
+            //将这组资源的id串联成字符串，值之间以逗号隔开
+
+            return CreatedAtRoute(nameof(GetCompanyCollection),
+                new {ids =idsString } ,
+                dtosToReturn);
         }
     }
 }
---
+++ Routine.Api/Helpers/ArrayModelBinder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Routine.Api.Helpers
+{
+    public class ArrayModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            //ModelBindingContext中有Model的源数据，用这些原数据来完成BindModelAsync这个方法
+            //由于get方法的参数是IEnumerable类型，所以需要保证ArrayModelBinder类作用域IEnumerable类型
+            //进行判断它所作用的类型不是IEnumerable类型时
+            if (!bindingContext.ModelMetadata.IsEnumerableType)
+            {
+                //返回失败
+                bindingContext.Result = ModelBindingResult.Failed();
+                return Task.CompletedTask;//
+            }
+
+            //否则就获取值（ValueProvider值提供者）
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).ToString();//获取到的就是逗号分隔开的字符串
+            
+            //判断字符串是否为空
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                //如果为空，则说明传入的参数为空，不需要进行其他特殊处理，此时是成功的
+                //应该在action中返回BadRequest
+                bindingContext.Result = ModelBindingResult.Success(null);
+                return Task.CompletedTask;//
+            }
+
+            //如果value值不是空的且它所作用的类型是IEnumerable类型
+            //首先需要获得IEnumerable中model的类型（在该代码中就是guid）
+            var elementType = bindingContext.ModelType.GetTypeInfo().GenericTypeArguments[0];//第0个为类型
+            //创建转换器
+            var converter = TypeDescriptor.GetConverter(elementType);//转换器为了转换字符串为Guid类型
+
+            //进行转换
+            var values = value.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                //Split函数的参数：第一个new[] { "," }表示新建一个数组，以","为分隔
+                //第二个参数：StringSplitOptions.RemoveEmptyEntries去掉为空的部分
+
+                //针对每一个元素使用转换器转换
+                .Select(x => converter.ConvertFromString(x.Trim())).ToArray();
+            //此时value返回的是一个object数组
+
+            //新建一个数组，转化为具体的类型
+            var typedValues = Array.CreateInstance(elementType, values.Length);//通过这个方法设定为相同长度
+            values.CopyTo(typedValues, 0);//values 复制到TypeValues 从第0个开始拷贝
+
+            bindingContext.Model = typedValues;//设置最终的返回结果
+            //此时bindingContext.Model的值就是IEnumerable<Guid>这个类型.
+
+            //最后返回成功
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+
+            return Task.CompletedTask;
+        }
+    }
+}
