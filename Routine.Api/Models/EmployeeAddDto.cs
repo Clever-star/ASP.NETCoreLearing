45458d3638591e9cf65d8596359edf40680ec79e
Clever-star <1908282601@qq.com>
2021-02-03 21:12:06 +0800

    P27

--- Routine.Api/Models/EmployeeAddDto.cs
+++ Routine.Api/Models/EmployeeAddDto.cs
@@ -1,19 +1,38 @@
 ﻿using Routine.Api.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Routine.Api.Models
 {
     public class EmployeeAddDto
     {
-        //在创建资源的Dto中不需要Id属性，该服务是由服务端实现的，而不是由客户端输入的
-        //public Guid CompanyId { get; set; }//外键，因为在方法参数中已经有了companyid这个参数，它们的值如果不同还需要进行判断操作，这种操作是冗余的
+        [Display(Name = "员工号")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "{0}的长度是{1}")]
         public string EmployeeNo { get; set; }
+
+        [Display(Name = "名")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [MaxLength(50, ErrorMessage = "{0}的长度不能超过{1}")]
         public string FirstName { get; set; }
+
+        [Display(Name = "姓"), Required(ErrorMessage = "{0}是必填项"), MaxLength(50, ErrorMessage = "{0}的长度不能超过{1}")]
         public string LastName { get; set; }
-        public Gender Gender { get; set; }//使用性别类
+
+        [Display(Name = "性别")]
+        public Gender Gender { get; set; }
+
+        [Display(Name = "出生日期")]
         public DateTime DateOfBirth { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (FirstName == LastName)
+            {
+                //错误信息与引起错误的位置
+                yield return new ValidationResult("姓和名不能一样", new[] { nameof(LastName), nameof(FirstName) });
+            }
+        }
     }
 }
